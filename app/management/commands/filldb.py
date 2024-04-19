0e3320bdb8a4bb3e813e60ce3867f8e372485fb9
jojosoderqvist <glebkarpikhin@gmail.com>
2024-04-19 18:03:44 +0300

    refactor models, db

--- app/management/commands/filldb.py
+++ app/management/commands/filldb.py
@@ -1,4 +1,8 @@
+import os.path
+
+from askme_karpikhin.settings import STATIC_URL
 from django.core.management.base import BaseCommand
+from django.core.files import File
 from django.contrib.auth.models import User
 from app.models import *
 from faker import Faker
@@ -41,11 +45,13 @@ class Command(BaseCommand):
         User.objects.bulk_create(users)
 
         user_objects = User.objects.all()
-        profiles = [Profile(user=user) for user in user_objects]
+        profiles = [Profile(user=user, displayed_name=user.username,
+                            avatar=os.path.join(STATIC_URL, 'img/default_profilepic.jpg')) for user in
+                    user_objects]
         Profile.objects.bulk_create(profiles)
         profile_objects = Profile.objects.all()
 
-       # questions and answers
+        # questions and answers
         questions_list = []
         answers_list = []
 
@@ -54,7 +60,6 @@ class Command(BaseCommand):
                 user=random.choice(profile_objects),
                 title=f"{fake.sentence()[:random.randint(10, 20)]}?",
                 text=fake.text()[:random.randint(30, 100)],
-                views=random.randint(0, ratio / 2),
                 created_at=fake.past_date(),
                 updated_at=fake.date_time_this_year()
             )
@@ -84,25 +89,38 @@ class Command(BaseCommand):
         answer_objects = Answer.objects.all()
 
         # Ratings
-        ratings = []
-
+        question_rating = []
+        answer_rating = []
         for profile in profile_objects:
             rated_questions = random.sample(list(question_objects), k=random.randint(70, 140))
             for question in rated_questions:
-                ratings.append(Rating(
+                question_rating.append(QuestionRating(
                     user=profile,
                     question=question,
-                    value=random.choice([-1, 1])
+                    value=random.choice([-1, 0, 1])
                 ))
 
             rated_answers = random.sample(list(answer_objects), k=random.randint(70, 140))
             for answer in rated_answers:
-                ratings.append(Rating(
+                answer_rating.append(AnswerRating(
                     user=profile,
                     answer=answer,
-                    value=random.choice([-1, 1])
+                    value=random.choice([-1, 0, 1])
                 ))
+        QuestionRating.objects.bulk_create(question_rating)
+        AnswerRating.objects.bulk_create(answer_rating)
 
-        Rating.objects.bulk_create(ratings)
+        question_rating_objects = QuestionRating.objects.all()
+        answer_rating_objects = AnswerRating.objects.all()
+        for question_object in question_objects:
+            rating_sum = question_rating_objects.filter(question=question_object).aggregate(Sum('value'))
+            if rating_sum['value__sum']:
+                question_object.rating = rating_sum['value__sum']
+                question_object.save()
+        for answer_object in answer_objects:
+            rating_sum = answer_rating_objects.filter(answer=answer_object).aggregate(Sum('value'))
+            if rating_sum['value__sum']:
+                answer_object.rating = rating_sum['value__sum']
+                answer_object.save()
 
         self.stdout.write('Success')
--- app/models.py
+++ app/models.py
@@ -4,40 +4,21 @@ from django.db import models
 
 
 class QuestionManager(models.Manager):
-    def get_all(self):
-        return self.annotate(
-            total_rating=Sum(Case(When(rating__question__isnull=False, then='rating__value')), default=0),
-            answers_number=Count('answer', distinct=True))
-
-    def get_hot(self):
-        return self.get_all().order_by('-total_rating')
-
-    def get_latest(self):
-        return self.get_all().order_by('-created_at')
-
-    def get_by_id(self, question_id):
-        return self.get_all().get(pk=question_id)
-
-    def get_by_tag(self, tag_name):
-        return self.get_all().filter(tags__name=tag_name)
+    pass
 
 
 class AnswerManager(models.Manager):
-    def get_all(self):
-        return self.annotate(
-            total_rating=Sum(Case(When(rating__answer__isnull=False, then='rating__value')), default=0))
-
-    def get_answers_for_question(self, question_id):
-        return self.get_all().filter(question_id=question_id)
+    pass
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(null=True, blank=True)
+    displayed_name = models.CharField(max_length=100, unique=True)
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return self.name
@@ -46,8 +27,9 @@ class Tag(models.Model):
 class Question(models.Model):
     tags = models.ManyToManyField(Tag)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     text = models.CharField(max_length=1000)
+    rating = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = QuestionManager()
@@ -61,29 +43,32 @@ class Answer(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     text = models.CharField(max_length=1000)
     is_correct = models.BooleanField()
+    rating = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     objects = AnswerManager()
 
 
-class Rating(models.Model):
-    class RateEntity(models.IntegerChoices):
-        NOT_HELPFUL = -1, 'Not Helpful'
-        NEUTRAL = 0, 'Neutral'
-        HELPFUL = 1, 'Helpful'
+class RateEntity(models.IntegerChoices):
+    NOT_HELPFUL = -1, 'Not Helpful'
+    NEUTRAL = 0, 'Neutral'
+    HELPFUL = 1, 'Helpful'
 
+
+class QuestionRating(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, blank=True)
-    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True, blank=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     value = models.IntegerField(choices=RateEntity.choices)
 
     class Meta:
-        unique_together = ('user', 'question'), ('user', 'answer')
-        constraints = [
-            models.CheckConstraint(
-                check=models.Q(question__isnull=False, answer__isnull=True) | models.Q(question__isnull=True,
-                                                                                       answer__isnull=False),
-                name='rating_for_either_question_or_answer'
-            ),
-        ]
+        unique_together = ('user', 'question')
+
+
+class AnswerRating(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    value = models.IntegerField(choices=RateEntity.choices)
+
+    class Meta:
+        unique_together = ('user', 'answer')
