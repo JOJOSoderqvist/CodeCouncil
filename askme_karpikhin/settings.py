9f652a76c1327e45ef7951f3fea5e998cf56e883
Gleb Karpikhin <glebkarpikhin@gmail.com>
2024-12-02 23:01:12 +0300

    upd

--- askme_karpikhin/settings.py
+++ askme_karpikhin/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
+    BASE_DIR / 'static',
     BASE_DIR / "node_modules/bootstrap",
 ]
 LOGIN_URL = '/login'
